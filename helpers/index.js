fe22877568073b253c4ef9422cf53f9148bca883
kostiantyn-sagan <kstelephone8@gmail.com>
2021-11-26 17:34:07 +0300

    Добавляет helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,6 @@
+const crypto = require("crypto");
+const certificates = require("./certificates");
+
 const validate = ({ data, name, instance }) => {
   const { payload, meta } = data;
 
@@ -78,6 +81,7 @@ const validateFields = ({ data, name, instance }) => {
     "meta",
     "source",
     "algorithm",
+    "signature",
   ];
 
   for (const key in data) {
@@ -105,4 +109,51 @@ const validateFields = ({ data, name, instance }) => {
   }
 };
 
-module.exports = { validate, validateFields };
+class Crypter {
+  constructor({
+    password = "very!Strong_pa$$wordNotverYSecure",
+    algorithm = "aes-256-cbc",
+  }) {
+    this.password = password;
+    this.algorithm = algorithm;
+    this.encoding = "utf8";
+  }
+
+  encrypt(str) {
+    const iv = crypto.randomBytes(16);
+
+    const cipher = crypto.createCipheriv(
+      this.algorithm,
+      Buffer.from(this.password),
+      iv
+    );
+
+    let encrypted = cipher.update(str, this.encoding);
+
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    const data = `${iv.toString("hex")}:${encrypted.toString("hex")}`;
+
+    return data;
+  }
+
+  decrypt(source) {
+    const [iv, str] = source.split(":");
+
+    const cipher = crypto.createDecipheriv(
+      this.algorithm,
+      Buffer.from(this.password),
+      Buffer.from(iv, "hex")
+    );
+
+    let decrypted = cipher.update(Buffer.from(str, "hex"), this.encoding);
+
+    decrypted = Buffer.concat([decrypted, cipher.final()]);
+
+    const data = decrypted.toString();
+
+    return data;
+  }
+}
+
+module.exports = { validate, validateFields, Crypter, certificates };
